02fda921327f47dd32e8cc8cd8e5e6d58230ba7a
Timur Ahatov <42232243+tamco61@users.noreply.github.com>
2024-09-02 19:26:44 +0300

    Update taylor.fsx

--- taylor.fsx
+++ taylor.fsx
@@ -1,25 +1,52 @@
-// Print a table of a given function f, computed by taylor series
-
-// function to compute
-let f = sin
-
 let a = 0.0
-let b = 1.0
-let n = 10
+let b = 0.5
+let n = 10.
+let e = 0.001
 
-// Define a function to compute f using naive taylor series method
-let taylor_naive = f
+let f x = log((1.0 + x) / (1.0 - x))
+
+let rec loop i n func x =
+  if i <= n then
+    let x = func x
+    loop (i + 1.) n func x
+  else
+    x
+
+let m a b = a * b
+
+let pow x n = loop 1. n (m x) x
+
+let rec taylor x i e sum = 
+  let last_y = (pow x (2. * (i) + 1.)) / (2. * (i) + 1.)
+  let new_y = (pow x (2. * (i + 1.) + 1.)) / (2. * (i + 1.) + 1.)
+  if (abs(new_y - last_y) < e) then
+    (2.*(sum + last_y), i)
+  else
+    let sum = sum + last_y
+    taylor x (i + 1.) e sum
 
 
-// Define a function to do the same in a more efficient way
-let taylor = f
+let rec taylorSmart x last_y i e sum = 
+  let new_y = pow x 2. * last_y * (2.*(i - 1.) + 1.)  / (i * 2. + 1.)
+  if (abs(new_y - last_y) < e) then
+    (2. * sum, (i - 1.) / 2.)
+  else
+    let sum = sum + new_y
+    taylorSmart x new_y (i + 2.) e sum
+
+let osnova i = 
+  let x = a+(float i)/(float n)*(b-a)
+  let t, ti = taylor x 0. e 0.
+  let tS, tSi = taylorSmart x x 1. e x  
+  printfn "|%5.2f|  %10.6f|  %10.6f|   %10.0f|  %10.6f|   %10.0f|" x (f x) t ti tS tSi
+  i + 1.
+
 
 let main =
-   for i=0 to n do
-     let x = a+(float i)/(float n)*(b-a)
-     printfn "%5.2f  %10.6f  %10.6f   %10.6f" x (f x) (taylor_naive x) (taylor x)
-// make sure to improve this table to include the required number of iterations
-// for each of the methods
+    printfn "--------------------------------------------------------------------------"
+    printfn "|  x  |    f(x)    |   Naive    |    Iters    |   Smart    |    Iters    |"
+    printfn "--------------------------------------------------------------------------"
+    loop 0. n osnova 0.
+    printfn "--------------------------------------------------------------------------"
 
 main
-
